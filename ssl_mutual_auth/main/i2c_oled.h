c2b25b2436f759a36b5dada0fbc5754fb78f1b50
kiel5 <kienkm59@gmail.com>
2023-12-17 20:17:44 +0700

    oled_done

---
+++ ssl_mutual_auth/main/i2c_oled.h
@@ -0,0 +1,15 @@
+#ifndef I2C_OLED_H
+#define I2C_OLED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+
+void oled_init(void);
+void oled_display_text(char *data,int row,int length);
+void oled_display_horizental(char *data, int row, int length);
+#endif
